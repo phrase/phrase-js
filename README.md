fbfba18d44f68abee658b0678c257dd96df0b0b0
Phrase <support@phrase.com>
2020-10-01 14:49:11 +0000

    Deploying from  phrase/openapi@de76dc94

--- README.md
+++ README.md
@@ -179,6 +179,7 @@ Class | Method | HTTP request | Description
 *DistributionsApi* | **distributionShow** | **GET** /accounts/{account_id}/distributions/{id} | Get a single distribution
 *DistributionsApi* | **distributionUpdate** | **PATCH** /accounts/{account_id}/distributions/{id} | Update a distribution
 *DistributionsApi* | **distributionsList** | **GET** /accounts/{account_id}/distributions | List distributions
+*DocumentsApi* | **documentDelete** | **DELETE** /projects/{project_id}/documents/{id} | Delete document
 *DocumentsApi* | **documentsList** | **GET** /projects/{project_id}/documents | List documents
 *FormatsApi* | **formatsList** | **GET** /formats | List formats
 *GitHubSyncApi* | **githubSyncExport** | **POST** /github_syncs/export | Export from Phrase to GitHub
--- src/apis/DocumentsApi.ts
+++ src/apis/DocumentsApi.ts
@@ -19,7 +19,14 @@ import {
     DocumentToJSON,
 } from '../models';
 
+export interface DocumentDeleteRequest {
+    projectId: string;
+    id: string;
+    xPhraseAppOTP?: string;
+}
+
 export interface DocumentsListRequest {
+    projectId: string;
     xPhraseAppOTP?: string;
     page?: number;
     perPage?: number;
@@ -31,10 +38,61 @@ export interface DocumentsListRequest {
 export class DocumentsApi extends runtime.BaseAPI {
 
     /**
+     * Delete an existing document.
+     * Delete document
+     */
+    async documentDeleteRaw(requestParameters: DocumentDeleteRequest): Promise<runtime.ApiResponse<any>> {
+        if (requestParameters.projectId === null || requestParameters.projectId === undefined) {
+            throw new runtime.RequiredError('projectId','Required parameter requestParameters.projectId was null or undefined when calling documentDelete.');
+        }
+
+        if (requestParameters.id === null || requestParameters.id === undefined) {
+            throw new runtime.RequiredError('id','Required parameter requestParameters.id was null or undefined when calling documentDelete.');
+        }
+
+        const queryParameters: any = {};
+
+        const headerParameters: runtime.HTTPHeaders = {};
+
+        if (requestParameters.xPhraseAppOTP !== undefined && requestParameters.xPhraseAppOTP !== null) {
+            headerParameters['X-PhraseApp-OTP'] = String(requestParameters.xPhraseAppOTP);
+        }
+
+        if (this.configuration && (this.configuration.username !== undefined || this.configuration.password !== undefined)) {
+            headerParameters["Authorization"] = "Basic " + btoa(this.configuration.username + ":" + this.configuration.password);
+        }
+        if (this.configuration && this.configuration.apiKey) {
+            headerParameters["Authorization"] = this.configuration.apiKey("Authorization"); // Token authentication
+        }
+
+        const response = await this.request({
+            path: `/projects/{project_id}/documents/{id}`.replace(`{${"project_id"}}`, encodeURIComponent(String(requestParameters.projectId))).replace(`{${"id"}}`, encodeURIComponent(String(requestParameters.id))),
+            method: 'DELETE',
+            headers: headerParameters,
+            query: queryParameters,
+        });
+
+        return new runtime.TextApiResponse(response) as any;
+    }
+
+    /**
+     * Delete an existing document.
+     * Delete document
+     */
+    async documentDelete(requestParameters: DocumentDeleteRequest): Promise<any> {
+        const response = await this.documentDeleteRaw(requestParameters);
+        return await response.value();
+    }
+
+    /**
      * List all documents the current user has access to.
      * List documents
      */
     async documentsListRaw(requestParameters: DocumentsListRequest): Promise<runtime.ApiResponse<Array<Document>>> {
+        if (requestParameters.projectId === null || requestParameters.projectId === undefined) {
+            throw new runtime.RequiredError('projectId','Required parameter requestParameters.projectId was null or undefined when calling documentsList.');
+        }
+
         const queryParameters: any = {};
 
         if (requestParameters.page !== undefined) {
@@ -59,7 +117,7 @@ export class DocumentsApi extends runtime.BaseAPI {
         }
 
         const response = await this.request({
-            path: `/projects/{project_id}/documents`,
+            path: `/projects/{project_id}/documents`.replace(`{${"project_id"}}`, encodeURIComponent(String(requestParameters.projectId))),
             method: 'GET',
             headers: headerParameters,
             query: queryParameters,
