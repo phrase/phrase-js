5dea3fcd90f076318aeb7d1c641c202dc7ba1616
Phrase <support@phrase.com>
2026-09-17 08:41:56 +0000

    Deploying from  phrase/openapi@2f11766f

--- src/runtime.ts
+++ src/runtime.ts
@@ -161,7 +161,7 @@ export class Configuration {
           configuration.headers = {}
         }
 
-        configuration.headers["User-Agent"] = "OpenAPI-Generator/3.25.0/js"
+        configuration.headers["User-Agent"] = "OpenAPI-Generator/3.26.0/js"
     }
 
     get basePath(): string {
